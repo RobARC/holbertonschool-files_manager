ab02fb59b59a0189b8f3d0d513c559fcdcef0f9c
RobARC <rrondonc@gmail.com>
2022-05-01 13:11:33 -0500

    Uploading task 1

---
+++ utils/db.js
@@ -0,0 +1,36 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.client.connect();
+
+    this.db = this.client.db(database);
+    this.users = this.db.collection('users');
+    this.files = this.db.collection('files');
+  }
+
+  isAlive() {
+    if (this.client.isConnected) {
+      return false;
+    }
+    return true;
+  }
+
+  async nbUsers() {
+    const numbDocs = await this.db.collection('users');
+    return numbDocs.countDocuments();
+  }
+
+  async nbFiles() {
+    const numbDocs = await this.db.collection('files');
+    return numbDocs.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
