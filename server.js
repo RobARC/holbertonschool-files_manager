9eef556b63c20df56d6fe5634b534a15839d5400
RobARC <rrondonc@gmail.com>
2022-05-01 21:48:31 -0500

    uploading task2

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+// Initializer
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 5000);
+
+// Routes
+app.use('./routes', require('./routes/index'));
+
+// starting server
+
+app.listen(app.get('port', () => {
+  console.log('Server on port', app.get('port'));
+}));
