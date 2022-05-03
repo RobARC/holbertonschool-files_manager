9468c857f612a4c6afe3bacc69bb7533dcdf87e9
RobARC <rrondonc@gmail.com>
2022-05-03 11:42:47 -0500

    Update server

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.set('port', process.env.PORT || 5000);
 app.use(require('./routes/index'));
 
 // starting server
-
 app.listen(app.get('port', () => {
   console.log('Server running on port', app.get('port'));
 }));
