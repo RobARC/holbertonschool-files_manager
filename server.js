bf33d04f491596ba75252b74b1184316ef6727aa
RobARC <rrondonc@gmail.com>
2022-05-03 10:46:17 -0500

    Update server

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
 const express = require('express');
-const router = require('./routes/index')
 
 // Initializer
 const app = express();
 
 // Settings
 app.set('port', process.env.PORT || 5000);
+app.use(app.json());
 
 // Routes
-app.use(express.json());
-app.use(router);
+app.use(require('./routes/index'));
 
 // starting server
 
 app.listen(app.get('port', () => {
   console.log('Server running on port', app.get('port'));
 }));
-
-export default app;
