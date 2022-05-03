23626932d77a378d98da11d550a9362cbf20ad01
RobARC <rrondonc@gmail.com>
2022-05-03 11:07:00 -0500

    Update routes

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const AppController = require('../controllers/AppController');
 
 // app Routes
 
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
+router.get('/status',(req, res) => AppController.getStatus(req, res));
+router.get('/stats',(req, res) =>AppController.getStats(req, res));
 
 module.exports = router;
