06e6eecfcb847ec64cb54788a40c8a61f7be399f
RobARC <rrondonc@gmail.com>
2022-05-03 15:47:56 -0500

    uploading task 3

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,6 @@ const AppController = require('../controllers/AppController');
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
 
 module.exports = router;
